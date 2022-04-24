47dec8186bea4a56aeea25d39e7a5fe96e70f6f8
Ndimphiwe Mayekiso <nm.mayekiso@gmail.com>
2022-04-24 23:42:12 +0200

    refactor (app): Cleaned up folder structure and added grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { grid } from './config/grid';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'toy-robot';
+  table = {
+    rows: [...Array(grid.height).keys()].reverse(),
+    columns: [...Array(grid.length).keys()],
+    cellSize: grid.cellSize
+  };
+
+  isInTableBounds(xPosition: number, yPosition: number): boolean {
+    return xPosition >= 0 && xPosition < this.table.columns.length && yPosition >= 0 && yPosition < this.table.rows.length;
+  }
 }
---
+++ src/app/config/grid.ts
@@ -0,0 +1,5 @@
+export const grid = {
+    height: 5,
+    length: 5,
+    cellSize: 100
+}
