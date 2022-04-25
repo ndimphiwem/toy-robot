9375fc1da2b8ea9ae7eb1acc2735b4fc352805e2
Ndimphiwe Mayekiso <nm.mayekiso@gmail.com>
2022-04-26 00:18:04 +0200

    chor (tests): Added tests for robot service and app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,52 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { RobotService } from './robot/robot.service';
 
 describe('AppComponent', () => {
+  let mockedRobotService = jasmine.createSpyObj<RobotService>('RobotService', ['getInstructions', 'convertInstructions', 'isInTableBounds']);
+  let app: AppComponent;
+  const mockInstructions = [
+    "PLACE -2,2,NORTH",
+    "REPORT",
+    "PLACE 1,0,SOUTH",
+    "PLACE 2,4,NORTH",
+    "RIGHT",
+    "MOVE",
+    "MOVE",
+    "REPORT"
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      providers: [{
+        provide: RobotService,
+        useValue: mockedRobotService
+      }],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  beforeEach(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    mockedRobotService.getInstructions = jasmine.createSpy().and.returnValue(of(mockInstructions));
+    app = fixture.componentInstance;
+  });
+
+  it('should create the app component', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'toy-robot'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('toy-robot');
+  it('Should have called the getInstructions method of the Robot Service', () => {
+    app.getInstructions();
+    expect(mockedRobotService.getInstructions).toHaveBeenCalled();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('toy-robot app is running!');
-  });
+  it('Should have a list of instructions in an array format', () => {
+    app.getInstructions();
+    expect(mockedRobotService.getInstructions).toHaveBeenCalled();
+    expect(app.instructions.length).toBe(mockInstructions.length);
+  });  
 });
